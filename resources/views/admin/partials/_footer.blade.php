307050368491558d56445486587bbc0bdcd854f1
Nurbaitillah <nurbaitilah7@gmail.com>
2024-02-27 12:51:05 +0700

    UKK Web-galleryindri

---
+++ resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+    All Rights Reserved by SMK Al Hafidz Leuwiliang. Designed and Developed by <a href="https://instagram.com/indrriiww" target="_blank">Indri</a>.
+</footer>
